1e8eb53cf2f93e25adab99f6a471213a4136fcb6
ThibDaniel <160511547+ThibDaniel@users.noreply.github.com>
2024-07-23 11:18:05 +0200

    Update lucky.mjs

--- lucky.mjs
+++ lucky.mjs
@@ -11,3 +11,19 @@ function luckyDraw(player) {
     });
   });
 }
+
+luckyDraw('Joe')
+  .then(result => {
+    console.log(result);
+    return luckyDraw('Caroline');
+  })
+  .then(result => {
+    console.log(result);
+    return luckyDraw('Sabrina');
+  })
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.error(error.message);
+  });
